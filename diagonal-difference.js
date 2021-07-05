22a67bc0ce914c9744b59b4aac5ededcc3b5366f
hamcuks <ivannurilham60@gmail.com>
2021-07-05 12:35:02 +0700

    [+] add comment to hackerrank diagonal difference

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -8,7 +8,10 @@ let diagonalDifference = (arr) => {
     let a = 0;
     let b = 0;
     for (let i = 0; i < arr.length; i++) {
+        // index: 00, 01, 02
         a += arr[i][i]
+
+        // index: 02, 11, 20
         b += arr[i][(arr.length - 1) - i]
     }
 
